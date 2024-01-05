2ffcdd3e1af416c08671092a66cad66cac73432b
Zubair <zubair99ss@hotmail.com>
2024-01-05 23:25:35 +0800

    Update colour scheme and include clear grid button

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const DEFAULT_SIZE = 16
 
 let current_size = DEFAULT_SIZE
 
+const createGridBtn = document.getElementById('createGridBtn');
+const clearGridBtn = document.getElementById('clearGridBtn');
+const container = document.getElementById('container');
+
+createGridBtn.addEventListener('click', () => refreshGrid());
+clearGridBtn.addEventListener('click', () => clearGrid());
+
 // Function to create an n x n grid in the "container" div
 function createGrid(size) {
+    console.log("Creating Grid of size: " + size)
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`
   
@@ -16,19 +24,16 @@ function createGrid(size) {
 
 // Function to clear grid of content
 function clearGrid() {
+    console.log("Clearing Grid")
     container.innerHTML = ''
 }
 
+// Function to clear the grid of existing content and create a new one
 function refreshGrid() {
     let number = prompt("How many boxes per side?");
     clearGrid()
     createGrid(number)
 };
-
-const createGridBtn = document.getElementById('createGridBtn');
-const container = document.getElementById('container');
-
-createGridBtn.addEventListener('click', () => refreshGrid());
 
 window.onload = () => {
     createGrid(DEFAULT_SIZE)
