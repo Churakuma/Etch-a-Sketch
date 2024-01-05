1fc9cf50a2228eefc99c094ae6933888880096f1
Zubair <zubair99ss@hotmail.com>
2024-01-05 23:40:13 +0800

    Add functionality to color over a grid square
    Fix clear grid button to enable it to refresh the grid and maintain a
    grid of the current size instead of requiring user to create a new grid
    after clearing their current one.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const DEFAULT_SIZE = 16
+const DEFAULT_COLOR = '#333333'
 
 let current_size = DEFAULT_SIZE
+let current_color = DEFAULT_COLOR
 
 const createGridBtn = document.getElementById('createGridBtn');
 const clearGridBtn = document.getElementById('clearGridBtn');
@@ -9,15 +11,21 @@ const container = document.getElementById('container');
 createGridBtn.addEventListener('click', () => refreshGrid());
 clearGridBtn.addEventListener('click', () => clearGrid());
 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
 // Function to create an n x n grid in the "container" div
 function createGrid(size) {
     console.log("Creating Grid of size: " + size)
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`
   
-    for (let i = 0; i < size; i++) {
+    for (let i = 0; i < size * size; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square')
+        gridSquare.addEventListener('mouseover', changeColor)
+        gridSquare.addEventListener('mousedown', changeColor)
         container.appendChild(gridSquare);
     }
 }
@@ -26,6 +34,13 @@ function createGrid(size) {
 function clearGrid() {
     console.log("Clearing Grid")
     container.innerHTML = ''
+    createGrid(current_size)
+}
+
+// Function to change the color of the selected grid square
+function changeColor(e) {
+    if (e.type === 'mouseOver' && !mouseDown) return
+    e.target.style.backgroundColor = current_color
 }
 
 // Function to clear the grid of existing content and create a new one
